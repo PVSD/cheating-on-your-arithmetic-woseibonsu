a681c7300cd1ff1173271b96a1eab9029330b38d
woseibonsu <40476387+woseibonsu@users.noreply.github.com>
2018-09-06 00:55:38 -0400

    Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Tester test = new Tester();
+
+        System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + test.problemOne());
+        System.out.println("(179 + 21 + 10) / 7 + 181 = " + test.problemTwo());
+        System.out.println("10389 * 56 * 11 * 2246 = " + test.problemThree());
+
+        //Do we actually have to use two classes to do this? I would told we did but I don't see that in the textbook.
     }
 }
---
+++ src/com/company/Tester.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Tester {
+    int problemOne()
+    {
+        return 79 + 3 * (4 + 82 - 68) - 7 + 19;
+    }
+
+    int problemTwo()
+    {
+        return (179 + 21 + 10) / 7 + 181;
+    }
+
+    int problemThree()
+    {
+        return 10389 * 56 * 11 + 2246;
+    }
+}
