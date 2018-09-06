83b2ec9f1706087fe3660d09378ed54915e38bc5
woseibonsu <40476387+woseibonsu@users.noreply.github.com>
2018-09-06 01:00:16 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,6 @@ public class Main {
         System.out.println("(179 + 21 + 10) / 7 + 181 = " + test.problemTwo());
         System.out.println("10389 * 56 * 11 * 2246 = " + test.problemThree());
 
-        //Do we actually have to use two classes to do this? I would told we did but I don't see that in the textbook.
+        //Do we actually have to use two classes to do this? I would told we did, but I don't see that in the textbook.
     }
 }
